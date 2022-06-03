9c565e671d8e78c57da1c4697fbb8e71840bc251
Tobias O <93132363+jrngn@users.noreply.github.com>
2022-06-03 16:09:37 +0200

    ReInit
    APCGS reinitialization commit:
    Pruned old & sparce commits history,
    it did not document changes well enough to be of any use.
    
    Removed primary project from the solution due to poor code quality;
    will track changes in different (private) repo while refactoring.
    Also exluded subprojects temporarily;
    will add them back later after short QA pass.
    
    Currently exposes only limited tokenization capabilities of LexMachina,
    will refactor other parts of the solution later.
    LexMachina itself still requires smaller tasks to be performed,
    including code fixes, refactorization & documentation.

---
+++ Tests/Tests.APCGS.LexMachina/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests.APCGS.LexMachina")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests.APCGS.LexMachina")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b95e65fb-dd4b-4030-b9f6-a8671d5ea63b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/Tests.APCGS.LexMachina/TokenTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using APCGS.LexMachina;
+using System.Collections.Generic;
+
+namespace Tests.APCGS.LexMachina
+{
+  [TestClass]
+  public class TokenTests
+  {
+    [TestMethod]
+    public void Token_TokenGeneration()
+    {
+      LexUtils.Random = new Random(1337);
+      var numToken = new NumericLexToken();
+      //var ret = new List<string>();
+      for (int i = 0; i < 100; i++)
+      {
+        //ret.Add(LexMicroMachina.GenerateToken(numToken));
+        var lex = LexMicroMachina.GenerateToken(numToken);
+        Console.WriteLine($"({(lex.token != null ? "valid" : "invalid")}) {lex.contents} : {lex.value} ({lex.value.GetType()})");
+      }
+    }
+  }
+}
